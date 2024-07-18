38b078086da130a4957240b9a76f9b5bddc88446
Jakub Samotyja <jjsamotyja@gmail.com>
2024-07-19 01:03:12 +0200

    random song picker feature

--- script.js
+++ script.js
@@ -2,43 +2,71 @@
 import data from './data.json' with { type: 'json' };
 console.log(data);
 
-document.getElementById("searchbar").addEventListener("keyup", search_songs);
-document.getElementById("type").addEventListener("change", search_songs);
-document.getElementById("btncheck1").addEventListener("click", search_songs);
-
-  function search_songs() {
-    let searchinput = document.getElementById('searchbar').value
-    let showfileinput = document.getElementById('btncheck1').checked
-    let type = document.getElementById('type').value
-    searchinput = searchinput.toLowerCase();
+    document.getElementById("searchbar").addEventListener("keyup", search_songs);
+    document.getElementById("type").addEventListener("change", search_songs);
+    document.getElementById("btncheck1").addEventListener("click", search_songs);
+    document.getElementById("btnrefresh").addEventListener("click", clearForm);
+    document.getElementById("btnrandom").addEventListener("click", selectRandomSong);
     let x = document.querySelector('#list-holder');
-    x.innerHTML = ""
 
-    for (let i = 0; i < data.length; i++) {
-      let obj = data[i];
+    function search_songs() {
+        let searchinput = document.getElementById('searchbar').value
+        let showfileinput = document.getElementById('btncheck1').checked
+        let type = document.getElementById('type').value
+        searchinput = searchinput.toLowerCase();
+        x.innerHTML = ""
 
-      if (obj[type].toLowerCase().includes(searchinput)) {
+        for (let i = 0; i < data.length; i++) {
+            let obj = data[i];
+
+            if (obj[type].toLowerCase().includes(searchinput)) {
+                const row = document.createElement("tr");
+
+                const artistCell = document.createElement("td");
+                artistCell.style = "width:70%"
+                artistCell.textContent = `${obj.ARTIST} - ${obj.TITLE}`;
+                row.appendChild(artistCell);
+
+                const gameCell = document.createElement("td");
+                gameCell.style = "width:20%"
+                gameCell.textContent = obj.GAME.toUpperCase();
+                row.appendChild(gameCell);
+
+                if (showfileinput == true) {
+                    const dlcCell = document.createElement("td");
+                    dlcCell.style = "width:10%"
+                    dlcCell.textContent = obj.DLC.toUpperCase();
+                    row.appendChild(dlcCell);
+                }
+
+                x.appendChild(row);
+            }
+        }
+    }
+
+    function selectRandomSong() {
+        x.innerHTML = ""
+        const randomSong = data[Math.floor(Math.random() * data.length)];
+
         const row = document.createElement("tr");
 
         const artistCell = document.createElement("td");
         artistCell.style = "width:70%"
-        artistCell.textContent = `${obj.ARTIST} - ${obj.TITLE}`;
+        artistCell.textContent = `${randomSong.ARTIST} - ${randomSong.TITLE}`;
         row.appendChild(artistCell);
 
         const gameCell = document.createElement("td");
-        gameCell.style = "width:20%"
-        gameCell.textContent = obj.GAME.toUpperCase();
+        gameCell.style = "width:30%"
+        gameCell.textContent = randomSong.GAME;
         row.appendChild(gameCell);
 
-        if (showfileinput == true) {
-          const dlcCell = document.createElement("td");
-          dlcCell.style = "width:10%"
-          dlcCell.textContent = obj.DLC.toUpperCase();
-          row.appendChild(dlcCell);
-        }
-
         x.appendChild(row);
-      }
     }
-  }
-  search_songs();
+
+    function clearForm() {
+        x.innerHTML = ""
+        document.getElementById('searchbar').value = "";
+        search_songs();
+    }
+
+    clearForm();
